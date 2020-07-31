dc3d4dd02e7ac5fa10b7d2fc54e860f280176da6
木生 <455194872@qq.com>
2020-07-31 18:17:11 +0800

    External interface(#10) * Gets the amount of tokens to be released

--- contracts/DefiSafeMineInterface.sol
+++ contracts/DefiSafeMineInterface.sol
@@ -4,4 +4,5 @@ pragma solidity 0.6.0;
 interface DefiSafeMine {
     function startMine(uint256 userTotalAssets,address receiveAddress) external;
     function getTotalTokensOfMine()external view returns(uint256);
+    function getTotalTokensOfFree()external view returns(uint256);
 }
